18ffb2e14376c27940fcdb9fb60a2ed8af72ea57
A-ron <aronf32@gmail.com>
2020-02-09 15:47:37 -0500

    adding multiple api tokens

--- js.js
+++ js.js
@@ -1,16 +1,22 @@
-var temp=[];var i;var jew;var calcs=1;
+var temp=[];var i;var jew;var calcs=1;var random;
 //etf declarations
 var VFH; //finance
 var XSW; //tech services
 var XLK; //tech electronic
+var tokens = [
+"NqDdbdp3GzGKl81tNyNW4NiKV2TLRLZlxgb24d9msoWoE93fYV9iimaSgyzR",
+"CtE9BRvfbrygbjgMZorx5A8fhs0t1rBKOINinkWoG3aSNqLAp40jdELocr29",
+"8I7CM5zhRvpPuIPMJc8Gzl2Mb0xeZ6YmzsLRQnzcGimK8s4JfO3uqlkeQRzX",
+"dO2qWHjQEcNP0rOlarznhzN71tosnQhgG8ZWMyobHGJh6aK8l3ea1vEPBjid",
+];
 const url = new URL(
     "https://api.worldtradingdata.com/api/v1/stock"
 );
-function price(symbol) {
+function price(symbol, token) {
 jew="";
 let params = {
     "symbol": (symbol),
-    "api_token": "CtE9BRvfbrygbjgMZorx5A8fhs0t1rBKOINinkWoG3aSNqLAp40jdELocr29",
+    "api_token": (token),
 };
 Object.keys(params)
     .forEach(key => url.searchParams.append(key, params[key]));
@@ -23,7 +29,7 @@ fetch(url, {
 }
 function getPrice(symbols) {
 temp=[];
-price(symbols);
+price(symbols, (tokens[Math.floor(Math.random() * (tokens.length))]));
 setTimeout(function(){
 for (i = 0; i < (symbols.split(",").length); i++) {
 temp[i] = ((Object.values(jew[i]))[0])+"*"+((Object.values(jew[i]))[10]);
